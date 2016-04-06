094e9ca4388921ef9f1b9efe64b78af80defd5c4
Martin Veres <martin.veres95gmail.com>
2016-04-06 03:53:17 +0200

    Uradjeni su testovi za klasu Twitter, uocene greske, ispravljene, sada klasa prolazi sve testove.

--- Twitter/src/test/com/twitter/TestTwitter.java
+++ Twitter/src/test/com/twitter/TestTwitter.java
@@ -2,6 +2,8 @@ package test.com.twitter;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +21,16 @@ Twitter t;
 	@After
 	public void tearDown() throws Exception {
 		t = null;
+	}
+	@Test
+	public void testVratiSvePoruke() {
+		t.unesi("M", "AA");
+		t.unesi("T", "Ae");
+		LinkedList<TwitterPoruka> poruke = t.vratiSvePoruke();
+		assertEquals(poruke.get(0).getKorisnik(), "M");
+		assertEquals(poruke.get(1).getKorisnik(), "T");
+		assertEquals(poruke.get(0).getPoruka(), "AA");
+		assertEquals(poruke.get(1).getPoruka(), "Ae");
 	}
 	@Test
 	public void testUnesiOK() {
@@ -40,5 +52,37 @@ Twitter t;
 	public void testUnesiObaNull() {
 		t.unesi(null, null);
 		
+	}
+	@Test
+	public void testVratiPorukeOK() {
+		t.vratiPoruke(10, "Mar");
+	}
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testVratiPorukeNull() {
+		t.vratiPoruke(11, null);
+	}
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testVratiPorukePrazanString() {
+		t.vratiPoruke(11, "");
+	}
+	@Test()
+	public void testVratiPorukeNegativanMaxBrojStringOK() {
+		t.vratiPoruke(-6,"a");
+	}
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testVratiPorukeNegativanMaxBrojStringNull() {
+		t.vratiPoruke(-6,null);
+	}
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testVratiPorukeNegativanMaxBrojStringPrazan() {
+		t.vratiPoruke(-6,"");
+	}
+	@Test
+	public void testVratiPorukeVracaSeNizSaMaksBrojElemenata() {
+		TwitterPoruka[] ta = new TwitterPoruka[2];
+		t.unesi("Joca", "Mama lepo kuva.");
+		t.unesi("Joca", "Moja mama lepo kuva.");
+		t.unesi("Joca", "Milanova mama lepo kuva.");
+		ta = t.vratiPoruke(2, "M");
 	}
 }
